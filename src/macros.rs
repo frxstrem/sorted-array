4eebbb67d714a68c3656d69579be755eebd052b6
Fredrik Østrem <frx.apps@gmail.com>
2020-04-18 02:07:47 +0200

    Document sorted_vec![...]

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,9 @@
+/// Macro to easily create a sorted array.
+///
+/// `sorted_vec![...]` is expanded to <code>[Sorted::new]\(vec![...])</code>.
 #[macro_export]
 macro_rules! sorted_vec {
-    ($($tt:tt)*) => {
+    [$($tt:tt)*] => {
         $crate::Sorted::new(vec![$($tt)*])
     }
 }
